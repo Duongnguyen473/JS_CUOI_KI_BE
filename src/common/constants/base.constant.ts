f16952fbbd6a9259cde3616a247ec705ffbdee5c
Bot473Gaming <tungnguyenduong473@gmail.com>
2025-06-25 00:52:20 +0700

    modify strobjectid

--- src/common/constants/base.constant.ts
+++ src/common/constants/base.constant.ts
@@ -1,11 +1,17 @@
 import { applyDecorators } from '@nestjs/common/decorators/core/apply-decorators';
 import { Column } from 'sequelize-typescript/dist/model/column/column';
 import { DataType } from 'sequelize-typescript/dist/sequelize/data-type/data-type';
+import { v4 as uuidv4 } from 'uuid';
+export const generateObjectId = (): string => {
+  return uuidv4().replace(/-/g, '');
+};
 
 export const StrObjectId = () =>
   applyDecorators(
     Column({
+      type: DataType.STRING,
       primaryKey: true,
-      defaultValue: () => DataType.UUIDV4,
+      defaultValue: generateObjectId,
+      allowNull: false,
     }) as PropertyDecorator,
   );
