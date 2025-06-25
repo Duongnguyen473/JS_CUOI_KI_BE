bfe2689c1c464d2219f601bb2c449f7cbff58359
Bot473Gaming <tungnguyenduong473@gmail.com>
2025-06-25 08:37:36 +0700

    refactor tutor profile model to include table definition and update certificate field type

--- src/modules/profile/models/tutor-profile.model.ts
+++ src/modules/profile/models/tutor-profile.model.ts
@@ -1,9 +1,13 @@
 import { StrObjectId } from '@/common/constants/base.constant';
 import { BaseEntity } from '@/common/interfaces/base-entity.interface';
-import { BelongsTo, Column, ForeignKey, Model } from 'sequelize-typescript';
+import { BelongsTo, Column, DataType, ForeignKey, Model, Table } from 'sequelize-typescript';
 import { EducationLever, ExperienceYear } from '../common/constant';
 import { UserModel } from '@/modules/user/models/user.model';
+import { EntityTable } from '@/common/constants/entity.constant';
 
+@Table({
+  tableName: EntityTable.TUTOR_PROFILE,
+})
 export class TutorProfileModel extends Model implements BaseEntity {
   @StrObjectId()
   _id: string;
@@ -27,7 +31,9 @@ export class TutorProfileModel extends Model implements BaseEntity {
   @Column
   experience_year: ExperienceYear;
   
-  @Column
+  @Column({
+    type: DataType.ARRAY(DataType.STRING),
+  })
   certificate: string[];
   
   @Column
