ac993bf2144ad5b1404c1075b08bfced906672a7
Bot473Gaming <tungnguyenduong473@gmail.com>
2025-07-08 08:24:33 +0700

    update config

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -6,4 +6,9 @@ export const appConfig = () => ({
     secret: process.env.JWT_SECRET || 'default-secret',
     expiresIn: process.env.JWT_EXPIRES_IN || '7d',
   },
+  payos: {
+    clientId : process.env.PAYOS_CLIENT_ID || 'default-client-id',
+    apiKey: process.env.PAYOS_API_KEY || 'default-api',
+    checksumKey: process.env.PAYOS_CHECKSUM_KEY || 'default-checksum-key',
+  }
 });
